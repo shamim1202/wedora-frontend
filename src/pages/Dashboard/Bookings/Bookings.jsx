68f93f86e607f27c7c9eb4497f54635c3109108d
Md. Mehedi Hasan Khan <mehedi.farzu@gmail.com>
2025-12-17 22:13:30 +0600

    show all my bookings and also implement delete function in the my booking page

--- src/pages/Dashboard/Bookings/Bookings.jsx
+++ src/pages/Dashboard/Bookings/Bookings.jsx
@@ -1,11 +1,15 @@
 import { useQuery } from "@tanstack/react-query";
 import useAuth from "../../../hooks/useAuth";
 import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import { RiDeleteBin6Line } from "react-icons/ri";
+import { FiEdit } from "react-icons/fi";
+import { FaMagnifyingGlass } from "react-icons/fa6";
+import Swal from "sweetalert2";
 
 const Bookings = () => {
   const { user } = useAuth();
   const axiosSecure = useAxiosSecure();
-  const { data: bookings = [] } = useQuery({
+  const { data: bookings = [], refetch } = useQuery({
     queryKey: ["bookings", user?.email],
     queryFn: async () => {
       const res = await axiosSecure.get(`/bookings?email=${user.email}`);
@@ -13,11 +17,72 @@ const Bookings = () => {
     },
   });
 
+  const handleBookingDelete = async (id) => {
+    try {
+      Swal.fire({
+        title: "Are you sure?",
+        text: "You won't be able to revert this!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Yes, delete it!"
+      }).then((result) => {
+        if (result.isConfirmed) {
+
+          axiosSecure.delete(`/bookings/${id}`)
+            .then(res => {
+              if (res.data.deletedCount > 0) {
+                // Refetch bookings after deletion
+                Swal.fire({
+                  title: 'Deleted!',
+                  text: 'Your booking has been deleted.',
+                  icon: 'success'
+                });
+                refetch()
+              }
+            })
+        }
+      });
+    } catch (error) {
+      console.error("Error deleting booking:", error);
+    }
+  };
+
   return (
-    <div>
-      <h1 className="text-4xl font-semibold text-rose-800">
+    <div className="p-5 md:p-10">
+      <h1 className="text-2xl md:text-4xl font-bold text-rose-800 md:mb-10 mb-5">
         All of my booking Service : {bookings.length}
       </h1>
+      <div className="overflow-x-auto">
+        <table className="table">
+          {/* head */}
+          <thead>
+            <tr>
+              <th>SL.</th>
+              <th>Service Name</th>
+              <th>Address</th>
+              <th>Date</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {/* ----- Table Row ----- */}
+            {bookings.map((b, i) =>
+              <tr key={b._id} className="">
+                <th>{i + 1}</th>
+                <td>{b.serviceName}</td>
+                <td>{b.location}</td>
+                <td>{b.date}</td>
+                <td className="flex gap-1 md:gap-2">
+                  <button className="btn btn-sm md:btn-md btn-square bg-transparent hover:bg-blue-100 border border-blue-200"><FaMagnifyingGlass className="md:text-xl text-blue-500 font-semibold" /></button>
+                  <button className="btn btn-sm md:btn-md btn-square bg-transparent hover:bg-green-100 border border-green-200"><FiEdit className="md:text-xl text-green-500 font-semibold" /></button>
+                  <button onClick={() => handleBookingDelete(b._id)} className="btn btn-sm md:btn-md btn-square bg-transparent hover:bg-red-100 border border-red-200"><RiDeleteBin6Line className="md:text-xl text-red-500 font-semibold" /></button>
+                </td>
+              </tr>)}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
