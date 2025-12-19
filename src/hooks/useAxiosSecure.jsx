ac99a871659ed62543accd179dcafa6823ced70a
Md. Mehedi Hasan Khan <mehedi.farzu@gmail.com>
2025-12-19 12:45:37 +0600

    implement jwt token to verify user and navigate unauthorized user to login page

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -1,10 +1,43 @@
 import axios from "axios";
+import { useEffect } from "react";
+import useAuth from "./useAuth";
+import { useNavigate } from "react-router";
 
 const axiosSecure = axios.create({
   baseURL: "http://localhost:3000",
 });
 
 const useAxiosSecure = () => {
+  const { user, logOut } = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    // Intercept request to add authorization header------>
+    const reqInterceptor = axiosSecure.interceptors.request.use((config) => {
+      config.headers.Authorization = `Bearer ${user?.accessToken}`;
+      return config;
+    });
+
+    // Intercept response to handle errors (optional)------>
+    const resInterceptor = axiosSecure.interceptors.response.use(
+      (response) => response,
+      (error) => {
+        const status = error.response?.status;
+        if (status === 401 || status === 403) {
+          logOut().then(() => {
+            navigate("/login");
+          });
+        }
+        return Promise.reject(error);
+      }
+    );
+
+    // Eject interceptors on cleanup to prevent memory leaks ------>
+    return () => {
+      axiosSecure.interceptors.request.eject(reqInterceptor);
+      axiosSecure.interceptors.response.eject(resInterceptor);
+    };
+  }, [user]);
   return axiosSecure;
 };
 
